46646717604ff1b0264587ead79528bf8bf1c2f8
JoniLahd <jonzzah.lahdemaki@gmail.com>
2023-04-17 19:59:29 +0300

    kirjautuminen lisätty ja muutettu ohjelma.cs

---
+++ kirjautuminen.cs
@@ -0,0 +1,18 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+  
+  // Call your method here:
+  PrintPhrase();
+
+  }
+ 
+  // Write your method here:
+  public static void PrintPhrase()
+  {
+    Console.WriteLine("In a hole in the ground there lived a method");
+    
+  }
+    
+}
