1a2ac865696f3f2d52b7387ec1c7109b33d40ccd
JoniLahd <jonzzah.lahdemaki@gmail.com>
2023-04-17 20:26:01 +0300

    Better handling of username and passwd

---
+++ login.cs
@@ -0,0 +1,16 @@
+// login.cs
+using System;
+class Example {
+    static voic Main() {
+
+        string user ="vamk";
+        string password = "wolffintie";
+
+        if (user == "vamk" && password == "wolffintie")
+        {
+            Console.WriteLine("Login ok!");
+        } else {
+            Console.WriteLine("Login failed")
+        }
+        }
+    }
