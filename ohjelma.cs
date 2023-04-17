004f6506c2531d2e7ccdae3e6bb65d7925f4d2cb
JoniLahd <jonzzah.lahdemaki@gmail.com>
2023-04-17 19:51:50 +0300

    ensimmäinen commit

---
+++ ohjelma.cs
@@ -0,0 +1,17 @@
+using System;
+
+class Program {
+  public static int Sum(int number1, int number2, int number3, int number4)
+  {
+    int sum = number1 + number2 + number3 + number4;
+    return sum;
+    
+
+  }
+  
+  public static void Main (string[] args) {
+    // no need to touch this code this time:
+    int answer = Sum(4, 3, 6, 1);
+    Console.WriteLine("Sum: " + answer);
+  }
+}
